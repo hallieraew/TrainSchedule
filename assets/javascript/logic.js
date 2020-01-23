43b3841c6dd9868e6e9aa63d6f84ba3f0ae14a42
hallieraew <hallieweintraub@gmail.com>
2020-01-22 22:34:49 -0600

    html in place and somestyle plus pseudo coded some js

---
+++ assets/javascript/logic.js
@@ -0,0 +1,8 @@
+// empty variables for all inputs
+// click function for submit button that updates to the page and to the DB
+// get info from firebase and include it here
+// need to read up on momentjs to determine what time function for counting down when next train comes
+// need to order the trains in order of next one coming
+//  make sure time is listed in military time
+// frequency needs to be parsed to minutes as well - momentjs
+// 
